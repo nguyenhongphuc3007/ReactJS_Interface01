3c50d330b8400846a4f831e1519fdc3f200410be
nguyenhongphuc3007 <45628764+nguyenhongphuc3007@users.noreply.github.com>
2020-04-28 18:50:17 +0700

    Updates

---
+++ precache-manifest.128d6b06546b601438dab2fb35e74c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db5f68bc1e2f871c9d442beb48a26585",
+    "url": "/ReactJS_Interface01/index.html"
+  },
+  {
+    "revision": "c6fdabcae724687e9c16",
+    "url": "/ReactJS_Interface01/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "e25ea24ab77d1fcfa883",
+    "url": "/ReactJS_Interface01/static/js/2.b59d0585.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS_Interface01/static/js/2.b59d0585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6fdabcae724687e9c16",
+    "url": "/ReactJS_Interface01/static/js/main.da640bdb.chunk.js"
+  },
+  {
+    "revision": "398c6974ecc624b52fcc",
+    "url": "/ReactJS_Interface01/static/js/runtime-main.4875a77e.js"
+  }
+]);
